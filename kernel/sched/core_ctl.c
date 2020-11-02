840864803cac03a15a555381d09d7e7c71a79df2
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-11-02 13:28:03 -0400

    sched: core_ctl: stop userspace from messing it with shitty adjustments
    let kernel to force stop the rom from messing jitter to achieve low jitter
    
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -1255,6 +1255,7 @@ static int core_ctl_isolation_dead_cpu(unsigned int cpu)
 
 /* ============================ init code ============================== */
 
+#if 0
 static struct cluster_data *find_cluster_by_first_cpu(unsigned int first_cpu)
 {
 	unsigned int i;
@@ -1267,6 +1268,8 @@ static struct cluster_data *find_cluster_by_first_cpu(unsigned int first_cpu)
 	return NULL;
 }
 
+#endif
+#if 0
 static int cluster_init(const struct cpumask *mask)
 {
 	struct device *dev;
@@ -1337,6 +1340,8 @@ static int cluster_init(const struct cpumask *mask)
 	return kobject_add(&cluster->kobj, &dev->kobj, "core_ctl");
 }
 
+#endif
+#if 0
 static int __init core_ctl_init(void)
 {
 	struct sched_cluster *cluster;
@@ -1361,3 +1366,4 @@ static int __init core_ctl_init(void)
 }
 
 late_initcall(core_ctl_init);
+#endif
